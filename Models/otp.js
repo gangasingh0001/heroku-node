df315dcf5cfb6e8c41a4e7b516ab7eebd9d4cd76
Ganga Singh <gangasingh@Gangas-MacBook-Pro.local>
2019-11-22 15:42:38 +0530

    twilio added

---
+++ Models/otp.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+delete mongoose.connection.models['user']
+const Schema = mongoose.Schema
+const verification = new Schema({
+    email : String,
+    activeStatus:Boolean,
+    otp:Number
+})
+
+const verify = mongoose.model('verification', verification)
+
+module.exports = {
+    verify
+}
