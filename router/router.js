7c36eb57fc86a301dd1c3e5b849f06818d036738
Ashish Rawat <ashish1198.cse16@chitkara.edu.in>
2019-11-25 15:22:27 +0530

    upload from excel

--- router/router.js
+++ router/router.js
@@ -56,12 +56,10 @@ module.exports = () => {
     })
     // For uploading questions directly from excel file
     app.post('/exam/questions/uploadExcel',middleware, upload.single('excelFile'), (req, res) => {
-        if(req.headers.role=="Examiner")
-        {
-        Users.quesFromExcel(req, res)
-        }
-        res.status(401).send('unauthorized')
 
+        
+        Users.quesFromExcel(req, res)
+        
     })
 
     app.post('/exam/accessKey', middleware, async(req, res) => {
